0f7b7e86fbef41d0e3912a16ae26f8ae1912f0f9
zhaofeihao <since_al@hotmail.com>
2023-04-02 16:18:22 +0800

    feat: 替换logo和配置

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,15 +2,15 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Brian Zhao\'s Homepage',
+  author: 'Brian Zhao',
+  headerTitle: '',
+  description: 'Something random and interesting.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
+  siteLogo: '/static/images/a.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
